39b7c7cf08a9bcb4a1313cd62696b39247a6ff11
Patrick Sinon <sinonpr@gmail.com>
2017-03-29 15:33:33 -0600

    Finalized implementation and tests

--- src/main/java/Expression.java
+++ src/main/java/Expression.java
@@ -2,4 +2,8 @@ package main.java;
 
 public interface Expression {
 	public Money reduce(Bank bank, String to);
+	
+	public Expression plus(Expression addend);
+	
+	public Expression times(int multiplier);
 }
